31a090724ce2bf4e156790f9af826ef69cb5a6f5
syjoe <csy5501@gmail.com>
2025-04-23 10:43:38 +0900

    Edit: navigation

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import api from "@/lib/api"
 import { RegisterRequest } from "@/types";
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
     const [form, setForm] = useState<RegisterRequest>({ username: '', email: '', password: '' });
